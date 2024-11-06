a68cd6a300b1192b62e08cd3772bdf9fe4dce6c0
weekdaycare <xiabee@duck.com>
2024-11-06 17:58:00 +0800

    [test] mail test

--- generator/main.py
+++ generator/main.py
@@ -52,24 +52,52 @@ def getData(repo, parameter, sort, data_pool, json_pool):
                         print(source)
                         data_pool.append(source)
 
-                    # 获取 mail-subscribe 的值
-                    mail_link = issues_soup.find('a', href=lambda x: x and x.startswith('mailto:'))
-                    if mail_link:
-                        mail_value = mail_link['href'].replace('mailto:', '')
-                        if mail_value:  # 过滤空值
-                            data_pool[-1]['mail-subscribe'] = mail_value
-
                 except Exception as e:
                     continue
     except Exception as e:
         print('> end')
     json_pool.append(data_pool)
 
+# 获取邮件订阅issue
+def getMail(repo, email_list):
+    try:
+        for number in range(1, 100):
+            linklist = []
+            print('page:', number)
+            url = f'https://github.com/{repo}/issues?page={number}&q=is%3Aopen+label%3Asubscribe'
+            print('parse:', url)
+            github = request.get_data(url)
+            soup = BeautifulSoup(github, 'html.parser')
+            main_content = soup.find_all('div', {'aria-label': 'Issues'})
+            if len(main_content):
+                linklist = main_content[0].find_all('a', {'class': 'Link--primary'})
+            if len(linklist) == 0:
+                print('> end')
+                break
+            for item in linklist:
+                issueslink = baselink + item['href']
+                issues_page = request.get_data(issueslink)
+                issues_soup = BeautifulSoup(issues_page, 'html.parser')
+
+                try:
+                    # 解析 email 地址
+                    mail_link = issues_soup.find('a', href=lambda x: x and x.startswith('mailto:'))
+                    mail_value = mail_link['href'].replace('mailto:', '')
+                    email_list.append(mail_value)
+                except Exception as e:
+                    continue
+    except Exception as e:
+        print('> end')
+
 def github_issuse(json_pool):
     print('\n')
     print('------- github issues start ----------')
     cfg = config.load()
     filter = cfg['issues']
+    subscribe = cfg['rss_subscribe']['enable']
+
+    if subscribe:
+        getMail(filter["repo"], email_list)
 
     if not filter["groups"]:
         # 如果没有配置groups，全部输出至data.json
@@ -92,6 +120,7 @@ def github_issuse(json_pool):
     print('\n')
 
 # 友链规则
+email_list = []
 github_issuse(json_pool)
 mkdir(outputdir)
 full_path = []
@@ -104,6 +133,6 @@ for filename in filenames:
     i += 1
 
 # 输出 subscribe.json
-subscribe_list = [item.get('mail-subscribe', '') for sublist in json_pool for item in sublist if item.get('mail-subscribe', '')]
+email_data = {"emails": email_list}
 with open(outputdir + '/subscribe.json', 'w', encoding='utf-8') as file_obj:
-    json.dump({"emails": subscribe_list}, file_obj, ensure_ascii=False, indent=2)
+    json.dump(email_data, file_obj, ensure_ascii=False, indent=2)
